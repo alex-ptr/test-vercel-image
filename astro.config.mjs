0023485b52b01483a74a4d69ea3ba6e958984493
Alexandre Peter <contact@alexandrepeter.fr>
2024-05-29 17:37:54 +0200

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,27 @@
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   adapter: vercel({
     imageService: true,
-    devImageService: "sharp",
+    /*     devImageService: "sharp", */
     imagesConfig: {
       sizes: [500, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-      domains: ["via.placeholder.com", "app.atelier-jbp.fr"],
-      /*       remotePatterns: [
+      domains: ["via.placeholder.com", "app.atelier-jbp.fr", "localhost:8055"],
+      remotePatterns: [
+        {
+          protocol: "http",
+          hostname: "localhost",
+          port: "8055",
+        },
         {
           protocol: "https",
           hostname: "via.placeholder.com",
-          pathname: "/500x500/",
-        }, 
-      ],*/
+        },
+      ],
     },
   }),
 });
