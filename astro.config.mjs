2e88647edcb64d5ab6e554dfafa3cab0c7cfc1c6
Alexandre Peter <contact@alexandrepeter.fr>
2024-05-29 17:41:37 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ export default defineConfig({
   output: "static",
   adapter: vercel({
     imageService: true,
-    /*     devImageService: "sharp", */
     imagesConfig: {
       sizes: [500, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
       domains: ["via.placeholder.com", "app.atelier-jbp.fr", "localhost:8055"],
