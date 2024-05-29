3b4d8c4ecc0bf3ae4726cd5d0270f090f8ba6892
Alexandre Peter <contact@alexandrepeter.fr>
2024-05-29 16:31:59 +0200

    add domains

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,23 +9,13 @@ export default defineConfig({
     imageService: true,
     devImageService: "sharp",
     imagesConfig: {
-      //domains: ["localhost", "placeholder.com"],
       sizes: [500, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      domains: ["placeholder.com"],
       remotePatterns: [
         {
           protocol: "https",
           hostname: "^via\\.placeholder\\.com$",
           pathname: "^/500x500/.*$",
-        },
-        {
-          protocol: "http",
-          hostname: "localhost",
-          port: 8055,
-        },
-        ,
-        {
-          protocol: "https",
-          hostname: "app.atelier-jbp.fr",
         },
       ],
     },
